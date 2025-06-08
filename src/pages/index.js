7063e3ba80033fc39ebe7e053b8489355ddb3af0
PoeticWater <aryanasajith@gmail.com>
2025-06-08 17:53:33 +0530

    Final-Test

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,9 +5,12 @@ export default function Home() {
     <div>
 
       <h1>Welcome to HealthMate</h1>
+      <p>Your AI symptom checker</p>
+      <p>Please select any of the two options given below</p>
 
       <nav>
         <Link href="/about">About Us</Link>
+
         <Link href="/contact">Contact Us</Link>
       </nav>
 
